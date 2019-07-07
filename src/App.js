be4b642b9a9638c485b3e36914d948806b0573be
Haroen Viaene <hello@haroen.me>
2019-07-07 21:48:35 +0200

    more styling

--- src/App.js
+++ src/App.js
@@ -26,9 +26,24 @@ function Form({ name, number }) {
           id="comments"
           name="comments"
           rows="5"
-          placeholder="e.g. I would prefer if the food is vegetarian and the venue is wheelchair accessible"
+          placeholder="e.g. “I would prefer if the food is vegetarian and the venue is wheelchair accessible.”"
         />
       </div>
+
+      <fieldset className="inline-radio">
+        <legend>RSVP</legend>
+        <div className="inner">
+          <input required type="radio" name="rsvp" id="yes" />
+          <label htmlFor="yes">Will come</label>
+          <hr />
+          <input required type="radio" name="rsvp" id="maybe" />
+          <label htmlFor="maybe">Might come</label>
+          <hr />
+          <input required type="radio" name="rsvp" id="no" />
+          <label htmlFor="no">Won’t come</label>
+        </div>
+      </fieldset>
+
       <div className="input-group">
         <input type="submit" value="Send" />
       </div>
@@ -39,20 +54,19 @@ function Form({ name, number }) {
 function App({ name, number }) {
   return (
     <div className="App">
-      <h1 className="header futura">Abi &amp; Haroen are getting married</h1>
-      <img src={image} alt="us together, looking amazing" className="border" />
+      <h1 className="futura">You are invited</h1>
       <p className="script subtitle">{name}</p>
-      <p className="futura">You are invited</p>
+      <p className="futura">to the wedding of</p>
+      <p className="subtitle script">Abi &amp; Haroen</p>
+      <img src={image} alt="us together, looking amazing" className="border" />
+      <p className="futura">Early August</p>
+      <p className="futura medium">2021</p>
+      <hr />
       <p>
-        The date and venue are not yet decided, but we know it will be around
-        the beginning of August 2021, and somewhere in France so all of you will
+        Of course it's still too early now to decide on an exact date or venue,
+        but we would love to have you attend! We know it will be around the
+        beginning of August 2021, and somewhere in France so all of you will
         have to travel a bit.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum iste saepe,
-        temporibus unde quia totam? Ratione sapiente, aut rem consectetur
-        recusandae molestias molestiae, consequatur labore, quibusdam enim nemo
-        ut sunt!
       </p>
       <Form name={name} number={number} />
     </div>
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <App name={'olala & yass'} number={2} />,
+  <App name={'Jackie & Ivan'} number={2} />,
   document.getElementById('root')
 );
 
