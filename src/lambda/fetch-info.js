45e3a2b096d2f158b27663af65887eab052d5c82
Haroen Viaene <hello@haroen.me>
2019-07-10 00:00:10 +0200

    fix(λ): catch errors

--- src/lambda/fetch-info.js
+++ src/lambda/fetch-info.js
@@ -5,14 +5,21 @@ dotenv.config();
 const { AIRTABLE_API_KEY, AIRTABLE_BASE } = process.env;
 
 export async function handler(event, context) {
-  var base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base(AIRTABLE_BASE);
+  try {
+    const base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base(AIRTABLE_BASE);
 
-  const {
-    fields: { name, 'number of people': number, language },
-  } = await base('Invitations').find('recu1wxdt7GUx00qJ');
+    const {
+      fields: { name, 'number of people': number, language },
+    } = await base('Invitations').find('recu1wxdt7GUx00qJ');
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ name, number, language }),
-  };
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ name, number, language }),
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(e),
+    };
+  }
 }
