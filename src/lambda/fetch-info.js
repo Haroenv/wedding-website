79a42aff91a37ebbb8bcc77d4f739c9f54d3ad60
Haroen Viaene <hello@haroen.me>
2019-07-09 23:46:44 +0200

    chore(function): extract base in env

--- src/lambda/fetch-info.js
+++ src/lambda/fetch-info.js
@@ -1,15 +1,18 @@
-import Airtable from "airtable";
-import dotenv from "dotenv"
-dotenv.config()
+import Airtable from 'airtable';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const { AIRTABLE_API_KEY, AIRTABLE_BASE } = process.env;
 
 export async function handler(event, context) {
-  var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base('appz9qTGdIpJRyMz2');
+  var base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base(AIRTABLE_BASE);
 
-  const { fields: { name, ["number of people"]: number, language } } = await base('Invitations').find('recu1wxdt7GUx00qJ')
+  const {
+    fields: { name, 'number of people': number, language },
+  } = await base('Invitations').find('recu1wxdt7GUx00qJ');
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ name, number, language })
-  }
-
-} 
+    body: JSON.stringify({ name, number, language }),
+  };
+}
