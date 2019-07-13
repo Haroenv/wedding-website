f8a3ba222f1d7188acab0914ac2fb59384191e7c
Haroen Viaene <hello@haroen.me>
2019-07-13 16:54:43 +0200

    better error handling

--- src/index.tsx
+++ src/index.tsx
@@ -9,22 +9,44 @@ type Data = {
   language: string[];
 };
 
+const errorSubject = 'Received an error signing up';
+const emailAddress = 'help@abi-and-haroen.com';
+const errorMailto = `mailto:${emailAddress}?subject=${encodeURIComponent(
+  errorSubject
+)}`;
+
 function Wrapper() {
   const [data, setData] = useState<Data | undefined>(undefined);
   const [error, setError] = useState<Error | null>(null);
 
   useEffect(() => {
-    fetch('/.netlify/functions/fetch-info')
+    const url = new URL(window.location.href);
+    url.pathname = '/.netlify/functions/fetch-info';
+    fetch(url.href)
+      .then(res => {
+        if (res.ok) {
+          return res;
+        } else {
+          throw new Error('could not retrieve user');
+        }
+      })
       .then(res => res.json())
       .then(res => setData(res))
       .catch(err => setError(err));
   }, []);
-
+  if (error) {
+    return (
+      <>
+        <p>Sorry, </p>
+        <p>{error.message}</p>
+        <p>
+          Please email us: <a href={errorMailto}>help@abi-and-haroen.com</a>
+        </p>
+      </>
+    );
+  }
   if (!data) {
     return null;
-  }
-  if (error) {
-    return <>{error.message}</>;
   }
   return <App name={data.name} number={data.number} defaultLanguage={'en'} />;
 }
