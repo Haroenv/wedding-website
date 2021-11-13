9c2c8d8d28d34206351e4b2cb81a0da90a518485
Haroen Viaene <hello@haroen.me>
2021-11-13 14:39:37 +0100

    rsvp no more

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Rsvp from './Rsvp';
+import FirstRsvp from './FirstRsvp';
 import SaveTheDate from './SaveTheDate';
 import './index.css';
 import * as Reach from '@reach/router';
@@ -9,7 +9,7 @@ import Home from './Home';
 ReactDOM.render(
   <Reach.Router id="reach-root">
     <Home path="/" default />
-    <Rsvp path="/rsvp" />
+    <FirstRsvp path="/first-rsvp" />
     <SaveTheDate path="/save-the-date" />
   </Reach.Router>,
   document.getElementById('root')
